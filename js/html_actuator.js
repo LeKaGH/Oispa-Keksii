ba3044e58e37096a94524cc2161718cbc6c14375
LeKaGH <106396832+LeKaGH@users.noreply.github.com>
2023-05-27 14:27:58 +0300

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -13,7 +13,15 @@ function HTMLActuator() {
 
 }
 
-dogeSayings = [ 
+dogeSayings = [
+  'Fazer <3',
+  'Vitusti Sokeria!',
+  'Kaneli Ässä!',
+  'Filipinos <3',
+  'Pirkka!',
+  'Rainbow <3',
+  'Marabou on parasta!',
+  'Sokerihumala!'
 ]
 
 HTMLActuator.prototype.actuate = function (grid, metadata) {
